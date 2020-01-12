0fbe2c218ed5088dd2f7d434a06314b59d92c427
Giles Lloyd <giles.af.lloys@gmail.com>
2020-01-12 14:07:57 +0000

    test(profile): End-to-end test for the single profile endpoint

--- tests/profile.test.js
+++ tests/profile.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const SiteRepository = require('../src/Domain/Site/SiteRepository');
 
 beforeEach(() =>  {
     appVars = require('../app');
@@ -35,5 +36,25 @@ describe("End-to-end tests for profile endpoints", () => {
             .set('x-api-key', process.env.API_KEY);
 
         expect(res.statusCode).toEqual(404);
+    });
+
+    it("should return 1 valid result for profile by ID", async () => {
+        let sites = await (new SiteRepository()).getAll();
+
+        const res = await request(app)
+            .get('/profile/'+sites[0]._id)
+            .set('x-api-key', process.env.API_KEY);
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('meta');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.length).toEqual(1);
+
+        let profilingData = res.body.data[0];
+        let requiredKeys = ['title', 'url', 'response_code', 'score', 'error'];
+
+        for (let key in requiredKeys) {
+            expect(profilingData).toHaveProperty(requiredKeys[key]);
+        }
     });
 });
